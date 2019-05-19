01e02d080c4fddcae30e2855b7e6d7015d5348f5
Daniellera <fa.danial@gmail.com>
2019-05-19 15:37:36 +0430

    apple icon updated

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,1 +1,149 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){"use strict";r.r(t);const n={searchBtn:document.querySelector(".search-btn"),tagsSection:document.querySelector(".tags ul"),firstSection:document.querySelector(".first-section"),results:document.querySelector(".result-titles ul"),resultsSection:document.querySelector(".results-section")},o={"قورمه":["رب گوجه","جعفری","گوشت قرمز"],"پیتزا":["پنیر پیتزا","سیر","مرغ"],"ماست":["ماست"]};["رب گوجه","پیاز","سیر","سیب زمینی","ماست","مرغ","گوشت قرمز","پنیر پیتزا","جعفری","نان"].forEach(e=>{const t=`\n        <li>${e}</li>\n        `;n.tagsSection.insertAdjacentHTML("beforeend",t)}),document.querySelectorAll(".tags ul li").forEach(e=>{e.addEventListener("click",e=>{e.target.classList.toggle("tag-selected"),"tag-selected"==e.target.classList?e.target.dataset.selected=!0:e.target.dataset.selected=!1})});const c=e=>{n.firstSection.innerHTML="",n.resultsSection.style="block",(e=>{if(0===e.length){const e="<li>با این چیزایی که خونه داری باید تلفن را برداری زنگ بزنی رستوران</li>";n.results.insertAdjacentHTML("beforeend",e)}else e.forEach(e=>{const t=`\n                <li>${e}</li>\n            `;n.results.insertAdjacentHTML("beforeend",t)})})(e)};n.searchBtn.addEventListener("click",()=>{const e=(()=>{const e=[];return document.querySelectorAll(".tags ul li[data-selected=true]").forEach(t=>{e.push(t.textContent)}),e})();let t=[];Object.keys(o).forEach(r=>{let n=!0;for(var c=0;c<o[r].length;c++)if(!e.includes(o[r][c])){n=!1;break}n&&t.push(r)}),console.log(t),c(t)})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _views_base__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./views/base */ \"./src/js/views/base.js\");\n/* harmony import */ var _views_tagsView__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./views/tagsView */ \"./src/js/views/tagsView.js\");\n/* harmony import */ var _models_tags__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./models/tags */ \"./src/js/models/tags.js\");\n/* harmony import */ var _views_resultView__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./views/resultView */ \"./src/js/views/resultView.js\");\n\n//--Getting Tags\n\n//--Getting foods\n\n\n\n\n\n\n\n//--filling tags controller\nObject(_views_tagsView__WEBPACK_IMPORTED_MODULE_1__[\"tagsView\"])(_views_base__WEBPACK_IMPORTED_MODULE_0__[\"tags\"]);\n\n//--searching controller\n//determine the selected tags and highlight it\nObject(_views_tagsView__WEBPACK_IMPORTED_MODULE_1__[\"selectedTags\"])();\n\n\n\n\n//show Selected foods that matches the tags\nconst showSelectedFoods = el => {\n    //clear the UI\n    _views_resultView__WEBPACK_IMPORTED_MODULE_3__[\"removeUI\"]()\n    \n    //make new UI\n    _views_resultView__WEBPACK_IMPORTED_MODULE_3__[\"makeNewUI\"]()\n\n    //render result UI\n    _views_resultView__WEBPACK_IMPORTED_MODULE_3__[\"renderResultUI\"](el);\n    \n}\n\n//get data when pushing search button\n_views_base__WEBPACK_IMPORTED_MODULE_0__[\"elements\"].searchBtn.addEventListener('click',() =>{\n    //getting selected tags\n    const result = Object(_models_tags__WEBPACK_IMPORTED_MODULE_2__[\"collectSelectedTags\"])();\n    //--Matching selected tags and foods\n    let selectedOnes = [];\n    Object.keys(_views_base__WEBPACK_IMPORTED_MODULE_0__[\"foods\"]).forEach(el => {\n        let areSame = true;\n            for(var b=0;b<_views_base__WEBPACK_IMPORTED_MODULE_0__[\"foods\"][el].length;b++){\n                if(!result.includes(_views_base__WEBPACK_IMPORTED_MODULE_0__[\"foods\"][el][b])){\n                    areSame = false;\n                    break;\n                }\n             }  \n            if(areSame){\n                selectedOnes.push(el);\n            }\n    })\n    console.log(selectedOnes)\n    showSelectedFoods(selectedOnes);    \n})\n\n\n\n//# sourceURL=webpack:///./src/js/index.js?");
+
+/***/ }),
+
+/***/ "./src/js/models/tags.js":
+/*!*******************************!*\
+  !*** ./src/js/models/tags.js ***!
+  \*******************************/
+/*! exports provided: collectSelectedTags */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"collectSelectedTags\", function() { return collectSelectedTags; });\nconst collectSelectedTags = () => {\n    const collectedData = [];\n    document.querySelectorAll('.tags ul li[data-selected=true]').forEach(el => {\n        collectedData.push(el.textContent);\n    });\n    return collectedData;\n}\n\n//# sourceURL=webpack:///./src/js/models/tags.js?");
+
+/***/ }),
+
+/***/ "./src/js/views/base.js":
+/*!******************************!*\
+  !*** ./src/js/views/base.js ***!
+  \******************************/
+/*! exports provided: elements, tags, foods */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"elements\", function() { return elements; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"tags\", function() { return tags; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"foods\", function() { return foods; });\nconst elements = {\n    searchBtn: document.querySelector('.search-btn'),\n    tagsSection: document.querySelector('.tags ul'),\n    firstSection: document.querySelector('.first-section'),\n    results: document.querySelector('.result-titles ul'),\n    resultsSection: document.querySelector('.results-section')\n}\n\nconst tags = [\n   'رب گوجه',\n    'پیاز',\n    'سیر',\n    'سیب زمینی',\n    'ماست',\n    'مرغ',\n    'گوشت قرمز',\n    'پنیر پیتزا',\n    'جعفری',\n    'نان'\n];\n\nconst foods = {\n    قورمه: ['رب گوجه', 'جعفری', 'گوشت قرمز'],\n    پیتزا: ['پنیر پیتزا','سیر', 'مرغ'],\n    ماست: ['ماست']\n}\n\n//# sourceURL=webpack:///./src/js/views/base.js?");
+
+/***/ }),
+
+/***/ "./src/js/views/resultView.js":
+/*!************************************!*\
+  !*** ./src/js/views/resultView.js ***!
+  \************************************/
+/*! exports provided: removeUI, makeNewUI, renderResultUI */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"removeUI\", function() { return removeUI; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"makeNewUI\", function() { return makeNewUI; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"renderResultUI\", function() { return renderResultUI; });\n/* harmony import */ var _base__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base */ \"./src/js/views/base.js\");\n\n\nconst removeUI = () => {\n    _base__WEBPACK_IMPORTED_MODULE_0__[\"elements\"].firstSection.innerHTML = \"\";\n}\nconst makeNewUI = () => {\n    _base__WEBPACK_IMPORTED_MODULE_0__[\"elements\"].resultsSection.style = ('display','block');\n}\nconst renderResultUI = (el) => {\n    if(el.length === 0){\n        const markup = \"<li>با این چیزایی که خونه داری باید تلفن را برداری زنگ بزنی رستوران</li>\";\n        _base__WEBPACK_IMPORTED_MODULE_0__[\"elements\"].results.insertAdjacentHTML('beforeend',markup);\n    }else{\n        el.forEach(element => {\n            const markup = `\n                <li>${element}</li>\n            `\n            _base__WEBPACK_IMPORTED_MODULE_0__[\"elements\"].results.insertAdjacentHTML('beforeend',markup);\n        });\n    }\n\n}\n\n//# sourceURL=webpack:///./src/js/views/resultView.js?");
+
+/***/ }),
+
+/***/ "./src/js/views/tagsView.js":
+/*!**********************************!*\
+  !*** ./src/js/views/tagsView.js ***!
+  \**********************************/
+/*! exports provided: tagsView, selectedTags */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"tagsView\", function() { return tagsView; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"selectedTags\", function() { return selectedTags; });\n/* harmony import */ var _base__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base */ \"./src/js/views/base.js\");\n\nconst tagsView = (array) => {\n    array.forEach(element => {\n        const markup = `\n        <li>${element}</li>\n        `\n        _base__WEBPACK_IMPORTED_MODULE_0__[\"elements\"].tagsSection.insertAdjacentHTML('beforeend',markup);\n    });\n}\nconst selectedTags = () => {\n    document.querySelectorAll('.tags ul li').forEach(el => {\n        el.addEventListener('click', \n            e => {\n                e.target.classList.toggle('tag-selected');  \n                if(e.target.classList == 'tag-selected'){\n                    e.target.dataset.selected = true;\n                }else{\n                    e.target.dataset.selected = false;\n                }\n            })    \n    });\n};\n\n//# sourceURL=webpack:///./src/js/views/tagsView.js?");
+
+/***/ })
+
+/******/ });

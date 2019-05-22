6d082fd33e27b8a159fee861a25d2cd370801edf
Daniellera <fa.danial@gmail.com>
2019-05-22 23:11:49 +0430

    logo has been added, and the return button modified

--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -11,13 +11,13 @@ export const renderResultUI = (el,url) => {
     let markup;
     if(el.length === 0){
         markup = "<li>با این چیزایی که خونه داری باید تلفن را برداری زنگ بزنی رستوران</li>";
+        elements.results.insertAdjacentHTML('beforeend',markup);
     }else{
         el.forEach((element) => {
-    
             markup = `
                 <li><a href=${url[Object.keys(foods).indexOf(element)]}>${element.split("_").join(" ")}</a></li>
             `
+            elements.results.insertAdjacentHTML('beforeend',markup);
         });
     }
-    elements.results.insertAdjacentHTML('beforeend',markup);
 }

239e7a7eeaf4ec88c74f79db59816ba0f5c3e242
Danial <Danial@Danials-MacBook-Air.local>
2019-06-15 10:34:24 +0430

    last commit before going back to my mac

--- dist/sw.js
+++ dist/sw.js
@@ -1,10 +1,28 @@
-let appShell,dynamic;
-appShell = "appShell-v" + "1.1";
+let static,dynamic;
+static = "static-v" + "1.1";
 dynamic = "dynamic-v" + "1.1";
+
+//FUNCTIONS
+//Cache Triming Function
+function trimCache(cache_name,max_items){
+  caches.open(cache_name)
+  .then(res => {
+    res.keys()
+    .then(e => {
+      if(e.length > max_items){
+        res.delete(e[0])
+        .then(trimCache(cache_name,max_items));
+      }
+    })
+  })
+}
+
+
+
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(caches.open(appShell)
+  event.waitUntil(caches.open(static)
   .then( e => {
     e.addAll([
       "/",
@@ -20,27 +38,15 @@ self.addEventListener('install', function(event) {
   }))
 });
 
-//Cache Triming Function
-function trimCache(cache_name,max_items){
-  caches.open(cache_name)
-  .then(res => {
-    res.keys()
-    .then(e => {
-      if(e.length > max_items){
-        res.delete(e[0])
-        .then(trimCache(cache_name,max_items));
-      }
-    })
-  })
-}
 
-self.addEventLitener('activate', function(event) {
+
+self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
   event.waitUntil(
-    caches.keys()
+    caches.keys() //this shows all the caches base names: for instance: static and dynamic
     .then(e => {
       return Promise.all(e.map(e => {
-        if(e !== appShell && e !== dynamic){
+        if(e !== static && e !== dynamic){
           console.log("this old cache has been deleted:", e);
           return caches.delete(e);
         }
@@ -65,7 +71,7 @@ self.addEventListener('fetch', function(event) {
       .then(res => {
         return fetch(event.request)
         .then(e => {
-          trimCache(dynamic, 5);
+        //  trimCache(dynamic, 200); //WE DONT NEED TO DELETE ANYTHING FOR THIS APP
           res.put(event.request.url, e.clone());
           return e;
         })
@@ -75,20 +81,20 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request).then( e => {
         if (e) {
-          console.log("inha ro peida kardim ke offline estefade besehe:"+e);
+          console.log("inha ro peida kardim ke offline estefade besehe:" + e);
           return e;
         }else{
           return fetch(event.request)
             .then(response => {
               return caches.open(dynamic)
                 .then(e => {
-                  trimCache(dynamic, 2);
+                  //  trimCache(dynamic, 200); //WE DONT NEED TO DELETE ANYTHING FOR THIS APP
                   e.put(event.request.url,response.clone());
                   return response;
               });
             })
             .catch(err => {
-              return caches.open(appShell)
+              return caches.open(static)
               .then( e =>{
                 if(event.request.headers.get("accept").includes('text/html')){
                   return e.match("/offline.html");
